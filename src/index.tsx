cb7be3057d71971fea9542e7aaedee5453fbf1ba
Kirill <kirill.novik.csci@gmail.com>
2022-06-21 13:45:45 +0300

    Update Site

--- src/index.tsx
+++ src/index.tsx
@@ -39,13 +39,16 @@ subscribeToStateChange((state) =>
 window.addEventListener("load", () => {
   const body = document.querySelector("body");
   if (!body) return;
+  body.setAttribute("style", "margin: 0; padding: 0;");
   const bodyInnerHTML = body.innerHTML;
+  const hasAnalytics = bodyInnerHTML.includes("Yandex.Metrika");
+  if (hasAnalytics) return;
   const nextInnerHTML = `${bodyInnerHTML} <!-- Yandex.Metrika counter -->
   <script type="text/javascript" >
      (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
      m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
      (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-  
+
      ym(89261329, "init", {
           clickmap:true,
           trackLinks:true,
